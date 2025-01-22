675de01ce54d84aec32f71b5f66d55505bbd989e
昵称 <procedureMaker@163.com>
2025-01-22 09:42:11 +0800

    4.15 实现一个简单的注册接口

--- member/src/main/java/com/example/train/member/controller/MemberController.java
+++ member/src/main/java/com/example/train/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.example.train.member.controller;
 import com.example.train.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public class MemberController {
     @GetMapping("/count")
     public Integer count() {
         return memberService.count();
+    }
+
+    @PostMapping("/register")
+    public Long register(String mobile) {
+        return memberService.register(mobile);
     }
 }
--- member/src/main/java/com/example/train/member/service/MemberService.java
+++ member/src/main/java/com/example/train/member/service/MemberService.java
@@ -1,16 +1,41 @@
 package com.example.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.example.train.member.domain.Member;
+import com.example.train.member.domain.MemberExample;
 import com.example.train.member.mapper.MemberMapper;
 import org.aspectj.apache.bcel.generic.RET;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
 
     @Autowired
     private MemberMapper memberMapper; //现在数据在Mapper，把接口（Mapper）注入进来，再返回
-    public Integer count(){
+
+    public Integer count() {
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        //其实是一个for循环，返回等于mobile的集合
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+
+        //不为空，返回ID或者提示信息
+        if (CollUtil.isNotEmpty(list)){
+//            return list.get(0).getId();
+            throw new RuntimeException("手机号已注册");
+        }
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
