1a42de913423e22fe27c099f303bce6b0df509a2
昵称 <procedureMaker@163.com>
2025-02-02 15:13:59 +0800

    5.9 将后端接口改成body json方式传递参数，前后端联调成功

--- member/src/main/java/com/example/train/member/controller/MemberController.java
+++ member/src/main/java/com/example/train/member/controller/MemberController.java
@@ -8,10 +8,7 @@ import com.example.train.member.resp.MemberLoginResp;
 import com.example.train.member.service.MemberService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
@@ -42,7 +39,7 @@ public class MemberController {
     }
 
     @PostMapping("/send-code")
-    public CommonResp<Long> sendCode(@Valid MemberSendCodeReq memberSendCodeReq) {
+    public CommonResp<Long> sendCode(@Valid @RequestBody MemberSendCodeReq memberSendCodeReq) {
         memberService.sendCode(memberSendCodeReq);
         return new CommonResp<>();
     }
