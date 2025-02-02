b179614356750c8916e3a5b15fec9a293ce217e2
昵称 <procedureMaker@163.com>
2025-02-02 17:06:38 +0800

    5.13 vue组件开发 增加the-header组件

---
+++ web/src/components/the-header.vue
@@ -0,0 +1,37 @@
+<template>
+  <a-layout-header class="header">
+    <div class="logo"/>
+    <a-menu
+        v-model:selectedKeys="selectedKeys1"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">nav 1111</a-menu-item>
+      <a-menu-item key="2">nav 2</a-menu-item>
+      <a-menu-item key="3">nav 3</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script>
+import {defineComponent, reactive, ref} from "vue";
+
+
+export default defineComponent({
+  name: "the-header",
+  setup() {
+
+
+    return {
+      selectedKeys1: ref(['2']),
+    };
+  },
+});
+</script>
+
+<style scoped>
+//body{
+//  全局配置
+//}
+</style>
