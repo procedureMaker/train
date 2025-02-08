d9d83416241db3429c1585aeaef7de912165b4ab
昵称 <procedureMaker@163.com>
2025-02-08 17:57:48 +0800

    7.5 增加日志拦截器，统一打印日志流水号

--- member/src/main/java/com/example/train/member/config/SpringMvcConfig.java
+++ member/src/main/java/com/example/train/member/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.example.train.member.config;
 
+import com.example.train.common.interceptor.LogInterceptor;
 import com.example.train.common.interceptor.MemberInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +11,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SpringMvcConfig implements WebMvcConfigurer {
 
     @Autowired
+    private LogInterceptor logInterceptor;
+
+    @Autowired
     private MemberInterceptor memberInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);
+
         registry.addInterceptor(memberInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/member/hello","/member/send-Code","/member/login");
