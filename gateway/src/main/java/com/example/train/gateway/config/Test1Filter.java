bf5318e04fbfe8d6f7044914fdc8115a6a68780d
昵称 <procedureMaker@163.com>
2025-02-08 11:14:02 +0800

    6.7 演示gateway过滤器的使用

---
+++ gateway/src/main/java/com/example/train/gateway/config/Test1Filter.java
@@ -0,0 +1,21 @@
+package com.example.train.gateway.config;
+
+import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+@Component
+public class Test1Filter implements GlobalFilter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        LOG.info("Test1filter");
+//        让这个过滤器顺利的往后
+        return chain.filter(exchange);
+    }
+}
