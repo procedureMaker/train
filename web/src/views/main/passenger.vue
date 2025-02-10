f64aa9a222544bbf16738305a735c3a14793da47
昵称 <procedureMaker@163.com>
2025-02-10 22:32:09 +0800

    7.7 增加新增按钮，点击按钮弹出对话框

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,15 +1,30 @@
 <template>
   <h1>乘车人管理</h1>
+  <a-button type="primary" @click="showModal">新增</a-button>
+  <a-modal v-model:visible="visible" title="Basic Modal" @ok="handleOk">
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+  </a-modal>
 </template>
 
 <script>
-import {defineComponent} from "vue";
+import {defineComponent, ref} from "vue";
 export default defineComponent({
   name:'passenger-view',
   setup() {
-
+    const visible = ref(false);
+    const showModal = () => {
+      visible.value = true;
+    };
+    const handleOk = e => {
+      console.log(e);
+      visible.value = false;
+    };
     return{
-
+      visible,
+      showModal,
+      handleOk,
     };
   },
 });
