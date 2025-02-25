e8c9a1248e2be5b5f1079fb12298e90aa9330677
昵称 <procedureMaker@163.com>
2025-02-25 23:23:41 +0800

    7.10 乘车人列表查询界面开发，引入真实乘客数据

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,12 +1,12 @@
 <template>
-<!--  <a-button type="primary" @click="showModal">新增</a-button>-->
+  <!--  <a-button type="primary" @click="showModal">新增</a-button>-->
   <!--p防止与下面元素重叠 -->
   <p>
     <a-button type="primary" @click="showModal">新增</a-button>
   </p>
 
   <!--乘车人员展示-->
-  <a-table :dataSource="dataSource" :columns="columns" pagination="true"/>
+  <a-table :dataSource="passengers" :columns="columns" pagination="true"/>
   <!-- 新增弹窗 -->
   <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk" ok-text="确认" cancel-text="取消">
     <a-form
@@ -19,11 +19,11 @@
         @finishFailed="onFinishFailed"
     >
       <a-form-item label="姓名">
-        <a-input v-model:value="passenger.name" />
+        <a-input v-model:value="passenger.name"/>
       </a-form-item>
 
       <a-form-item label="身份证">
-        <a-input v-model:value="passenger.idCard" />
+        <a-input v-model:value="passenger.idCard"/>
       </a-form-item>
 
       <a-form-item label="类型">
@@ -38,7 +38,7 @@
 </template>
 
 <script>
-import {defineComponent, ref, reactive} from 'vue';
+import {defineComponent, ref, reactive, onMounted} from 'vue';
 import axios from "axios";
 import {notification} from "ant-design-vue";
 
@@ -68,30 +68,31 @@ export default defineComponent({
     };
     const handleOk = () => {
       axios.post("/member/passenger/save", passenger
-      ).then(response =>{
+      ).then(response => {
         let data = response.data;
-        if(data.success) {
-          notification.success({description:"乘车人基本信息保存成功"})
+        if (data.success) {
+          notification.success({description: "乘车人基本信息保存成功"})
           visible.value = false;
-        }else {
+        } else {
           notification.error({description: data.message})
         }
       })
     };
-    const dataSource = [
-      {
-        key: '1',
-        name: '胡彦斌',
-        age: 32,
-        address: '西湖区湖底公园1号',
-      },
-      {
-        key: '2',
-        name: '胡彦祖',
-        age: 42,
-        address: '西湖区湖底公园1号',
-      },
-    ];
+    const passengers = ref([]);
+    // const dataSource = [
+    //   {
+    //     key: '1',
+    //     name: '胡彦斌',
+    //     age: 32,
+    //     address: '西湖区湖底公园1号',
+    //   },
+    //   {
+    //     key: '2',
+    //     name: '胡彦祖',
+    //     age: 42,
+    //     address: '西湖区湖底公园1号',
+    //   },
+    // ];
     const columns = [
       {
         title: '姓名',
@@ -99,16 +100,41 @@ export default defineComponent({
         key: 'name',
       },
       {
-        title: '年龄',
-        dataIndex: 'age',
-        key: 'age',
+        title: '身份证',
+        dataIndex: 'idCard',
+        key: 'idCard',
       },
       {
-        title: '住址',
-        dataIndex: 'address',
-        key: 'address',
+        title: '类型',
+        dataIndex: 'type',
+        key: 'type',
       },
-    ]
+    ];
+    // handlerQuery 是一个执行异步网络请求的副作用函数（发起 HTTP 请求并更新响应式数据 passengers.value）。
+    // 它的核心目的是触发一个动作（发送请求并处理结果），而不是计算或返回一个值。
+    const handlerQuery = param => {
+      axios.get("/member/passenger/query-list", {
+        params: {
+          page: param.page,
+          size: param.size
+        }
+      }).then((response) => {
+        let data = response.data;
+        if (data.success) {
+          passengers.value = data.content.list;
+          console.log("1111", passengers)
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+
+    onMounted(() => {
+      handlerQuery({
+        page: 1,
+        size: 5
+      });
+    });
     return {
       visible,
       showModal,
@@ -116,8 +142,9 @@ export default defineComponent({
       passenger,
       onFinish,
       onFinishFailed,
-      dataSource,
-      columns
+      // dataSource,
+      columns,
+      passengers
     };
   },
 });
