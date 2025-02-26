091e2bef1fa59fef39229b32beb4dde466a4a928
昵称 <procedureMaker@163.com>
2025-02-26 08:44:01 +0800

    7.11 解决前后端交互Long类型精度丢失的问题

--- member/src/main/java/com/example/train/member/resp/PassengerQueryResp.java
+++ member/src/main/java/com/example/train/member/resp/PassengerQueryResp.java
@@ -1,14 +1,19 @@
 package com.example.train.member.resp;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToEmptyObjectSerializer;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 import lombok.ToString;
 
 import java.util.Date;
+
 @Data
 @ToString
 public class PassengerQueryResp {
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
-
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long memberId;
 
     private String name;
