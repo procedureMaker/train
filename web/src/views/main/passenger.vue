95c0d5ccc4c9a54ada092103622781c3375c4a36
昵称 <procedureMaker@163.com>
2025-02-28 22:29:22 +0800

    7.15 将下拉框的值提取成常量数组

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -50,9 +50,8 @@
 
       <a-form-item label="类型">
         <a-select v-model:value="passenger.type">
-          <a-select-option value="1">成人</a-select-option>
-          <a-select-option value="2">儿童</a-select-option>
-          <a-select-option value="3">学生</a-select-option>
+          <a-select-option v-for="item in PASSENGER_TYPE_ARRAY" :key="item.key" :value="item.key">{{ item.value }}
+          </a-select-option>
         </a-select>
       </a-form-item>
     </a-form>
@@ -67,6 +66,8 @@ import {notification} from "ant-design-vue";
 export default defineComponent({
   name: "passenger-view",
   setup() {
+    //将复选跟后端一样申请为枚举或常量
+    const PASSENGER_TYPE_ARRAY = [{key: "1", value: "成人"}, {key: "2", value: "儿童"}, {key: "3", value: "学生"}]
     const visible = ref(false);
     let passenger = ref({
       id: undefined,
@@ -186,6 +187,7 @@ export default defineComponent({
       });
     });
     return {
+      PASSENGER_TYPE_ARRAY,
       visible,
       onAdd,
       handleOk,
