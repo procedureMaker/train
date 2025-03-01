3eac29eb9031c643e092d7fd631f8389892af293
昵称 <procedureMaker@163.com>
2025-03-01 15:16:42 +0800

    8.4 集成dom4j，读取pom.xml，得到当前持久层的xml文件

--- generator/src/main/java/com/example/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/example/train/generator/server/ServerGenerator.java
@@ -2,6 +2,10 @@ package com.example.train.generator.server;
 
 import com.example.train.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,15 +14,26 @@ import java.util.*;
 public class ServerGenerator {
     static String toPath = "generator\\src\\main\\java\\com\\example\\train\\generator\\test\\";
 
+    static String pomPath = "generator/pom.xml";
+
     static {
         new File(toPath).mkdirs();
     }
 
-    public static void main(String[] args) throws IOException, TemplateException {
-        FreemarkerUtil.initConfig("test.ftl");
-        HashMap<String, Object> param = new HashMap<>();
-        param.put("domain", "Test");
-        FreemarkerUtil.generator(toPath + "Test.java", param);
+    private static String getGeneratorPath() throws DocumentException {
+        SAXReader saxReader = new SAXReader();
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("pom", "http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        //读取定义好的pom文件
+        Document document = saxReader.read(pomPath);
+        //使用XPath快速定位节点和属性  //：从根目录下寻找，pom是xml命名空间  configuration:在命名空间下寻找configuration的节点
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        System.out.println(node.getText());
+        return node.getText();
+    }
+    public static void main(String[] args) throws Exception {
+        getGeneratorPath();
     }
 }
 
