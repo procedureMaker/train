e4a59af5e265bdf7c3cdddeec36f5892988bdf1c
昵称 <procedureMaker@163.com>
2025-03-03 14:53:50 +0800

    9.3 admin去掉登录页面，去掉member和登录相关的拦截，去掉乘车人页面

--- admin/src/store/index.js
+++ admin/src/store/index.js
@@ -1,20 +1,11 @@
 import { createStore } from 'vuex'
 
-const MEMBER = "MEMBER"
-
 export default createStore({
   state: {
-    // {}避免空指针异常，有值就是缓存里的值
-    member: window.SessionStorage.get(MEMBER) || {}
   },
   getters: {
   },
   mutations: {
-    setMember(state,_member){
-      state.member = _member;
-      //属性有变动，更新，放到缓存里
-      window.SessionStorage.set(MEMBER,_member)
-    }
   },
   actions: {
   },
