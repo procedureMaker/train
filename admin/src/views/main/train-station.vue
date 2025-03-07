7b5f8ee2a5490bb24ce964a280739fbfc27c823f
昵称 <procedureMaker@163.com>
2025-03-07 15:54:29 +0800

    9.20 自动计算停车时长

--- admin/src/views/main/train-station.vue
+++ admin/src/views/main/train-station.vue
@@ -29,7 +29,7 @@
            ok-text="确认" cancel-text="取消">
     <a-form :model="trainStation" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
       <a-form-item label="车次编号">
-        <train-select-view v-model:value="trainStation.trainCode" ></train-select-view>
+        <train-select-view v-model:value="trainStation.trainCode"></train-select-view>
       </a-form-item>
       <a-form-item label="站序">
         <a-input v-model:value="trainStation.index"/>
@@ -47,7 +47,7 @@
         <a-time-picker v-model:value="trainStation.outTime" valueFormat="HH:mm:ss" placeholder="请选择时间"/>
       </a-form-item>
       <a-form-item label="停站时长">
-        <a-time-picker v-model:value="trainStation.stopTime" valueFormat="HH:mm:ss" placeholder="请选择时间"/>
+        <a-time-picker v-model:value="trainStation.stopTime" valueFormat="HH:mm:ss" placeholder="请选择时间" disabled/>
       </a-form-item>
       <a-form-item label="里程（公里）">
         <a-input v-model:value="trainStation.km"/>
@@ -63,6 +63,7 @@ import axios from "axios";
 import {pinyin} from "pinyin-pro";
 import TrainSelectView from "@/components/train-select.vue";
 import StationSelectView from "@/components/station-select.vue";
+import dayjs from 'dayjs';
 
 export default defineComponent({
   name: "train-station-view",
@@ -141,6 +142,18 @@ export default defineComponent({
         dataIndex: 'operation'
       }
     ];
+
+    //自动计算停车时长
+    watch(() => trainStation.value.inTime, () => {
+      let diff = dayjs(trainStation.value.outTime, 'HH:mm:ss').diff(dayjs(trainStation.value.inTime, 'HH:mm:ss'), 'second');
+      trainStation.value.stopTime = dayjs('00:00:00', 'HH:mm:ss').second(diff).format('HH:mm:ss');
+    }, {immediate: true});
+    //自动计算停车时长
+    watch(() => trainStation.value.outTime, () => {
+      let diff = dayjs(trainStation.value.outTime, 'HH:mm:ss').diff(dayjs(trainStation.value.inTime, 'HH:mm:ss'), 'second');
+      trainStation.value.stopTime = dayjs('00:00:00', 'HH:mm:ss').second(diff).format('HH:mm:ss');
+    }, {immediate: true});
+
 
     watch(() => trainStation.value.name, () => {
       if (Tool.isNotEmpty(trainStation.value.name)) {
