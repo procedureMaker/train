b0690420afba59099a2197484265e4fce4b5b1f1
昵称 <procedureMaker@163.com>
2025-03-07 20:10:43 +0800

    10.5 集成quarz和springboot自带的定时任务可以共存

---
+++ batch/src/main/java/com/example/train/batch/job/TestJob.java
@@ -0,0 +1,20 @@
+package com.example.train.batch.job;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class TestJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("TestJob TEST开始");
+        // try {
+        //     Thread.sleep(3000);
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        // }
+        System.out.println("TestJob TEST结束");
+    }
+}
